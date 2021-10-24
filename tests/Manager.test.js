5cb42c4cf0a8b675dd5622e5e6290c07fd4c2b75
Sri <srilalitha.ganti@gmail.com>
2021-10-24 13:47:00 +1100

    added tests for manager

---
+++ tests/Manager.test.js
@@ -0,0 +1,22 @@
+const Manager = require("../lib/Manager");
+describe("Manager", () => {
+  it("Can set office number via constructor argument", () => {
+    const testValue = 130;
+    const e = new Manager("Jim", 100, "test@test.com", testValue);
+    expect(e.officeNumber).toBe(testValue);
+  });
+  describe("getRole", () => {
+    it('getRole() should return "Manager"', () => {
+      const testValue = "Manager";
+      const e = new Manager("Jim", 100, "test@test.com", 130);
+      expect(e.getRole()).toBe(testValue);
+    });
+  });
+  describe("getOffice", () => {
+    it("Can get office number via getOffice()", () => {
+      const testValue = 130;
+      const e = new Manager("Jim", 100, "test@test.com", testValue);
+      expect(e.getOfficeNumber()).toBe(testValue);
+    });
+  });
+});
